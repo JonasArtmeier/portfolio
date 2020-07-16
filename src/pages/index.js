87d99b4d891787393fb039d2021656e6d9c5b1ca
Jonas Artmeier <jonas-artmeier@web.de>
2020-07-16 11:01:56 +0200

    back to avatar img

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 // import Gallery from '../components/Gallery'
 import Layout from '../components/layout'
 // import screenshot.jpg from '../assets/images/screenshot.jpg'
-import screenshot from '../assets/images/screenshot.jpg'
+// import screenshot from '../assets/images/screenshot.jpg'
 
 const avatar =
   'https://artmeier.me/static/avatar-1deef899cc5860533a54e00b17dc475e.png'
@@ -28,7 +28,10 @@ const HomeIndex = () => {
         />
 
         <meta property="og:title" content="Jonas Artmeier - Portfolio" />
-        <meta property="og:image" content={screenshot} />
+        <meta
+          property="og:image"
+          content={require('../assets/images/avatar.png')}
+        />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
         <meta property="og:image:alt" content="Jonas" />
@@ -42,15 +45,15 @@ const HomeIndex = () => {
           name="twitter:description"
           content="Find all inforamtion about Jonas Artmeier as an IT Strategy Consultant and Software Developer with a deep knowledge in Digital Transformation"
         />
-        <meta name="twitter:image" content={screenshot} />
+        <meta name="twitter:image" content={avatar} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="https://artmeier.me/" />
       </Helmet>
-      <img
+      {/* <img
         style={{ display: 'none' }}
         src={screenshot}
         alt="Titelbild Artikel"
-      ></img>
+      ></img> */}
       <div id="main">
         <section id="one">
           <header className="major">
